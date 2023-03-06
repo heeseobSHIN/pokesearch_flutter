1a84203627ce1375232705b35a8542d0da099272
heeseobSHIN <zergpower08@gmail.com>
2023-03-06 15:38:09 +0900

    add short detailPage, gotoDeatil

--- lib/list.dart
+++ lib/list.dart
@@ -1,9 +1,11 @@
-// ignore_for_file: prefer_const_constructors, unused_import, avoid_print, prefer_const_constructors_in_immutables, unused_element, dead_code, prefer_interpolation_to_compose_strings, annotate_overrides, prefer_typing_uninitialized_variables, unused_local_variable, sized_box_for_whitespace
+// ignore_for_file: prefer_const_constructors, unused_import, avoid_print, prefer_const_constructors_in_immutables, unused_element, dead_code, prefer_interpolation_to_compose_strings, annotate_overrides, prefer_typing_uninitialized_variables, unused_local_variable, sized_box_for_whitespace, non_constant_identifier_names
 // import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
+
+import 'package:pokesearch/listdetail.dart';
 
 class PokeApi {
   var results;
@@ -21,13 +23,13 @@ class PokeApi {
 
 Future<PokeApi> fetchAlbum() async {
   // 포켓몬 주소
-  final response = await http
-      .get(Uri.parse('https://pokeapi.co/api/v2/pokemon/?limit=1279'));
+  final response =
+      await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon/?limit=150'));
 
   if (response.statusCode == 200) {
     return PokeApi.fromJson(jsonDecode(response.body));
   } else {
-    throw Exception('Failed to load album');
+    throw Exception('Failed to load pokeApi');
   }
 }
 
@@ -38,9 +40,25 @@ class PoketList extends StatefulWidget {
   State<PoketList> createState() => _PoketListState();
 }
 
+//객체 생성 PokeApi.results
+// PokeApi pokeApi = PokeApi(results: pokeApi.results);
+
 class _PoketListState extends State<PoketList> {
   late Future<PokeApi> futureAlbum;
   late Future<PokeApi> futurepage;
+
+  // List<PokeApi> Presults = List.from(pokeApi.results);
+
+  // void updateList(String value) {
+  //   //fillter나 list 기능
+  //   setState(() {
+  //     Presults = pokeApi.results
+  //         .where((element) => element.pokeapi.results!
+  //             .toLowerCase()
+  //             .contains(value.toLowerCase()))
+  //         .toList();
+  //   });
+  // }
 
   @override
   void initState() {
@@ -64,8 +82,9 @@ class _PoketListState extends State<PoketList> {
                 ),
                 child: Center(
                   child: TextField(
+                    // onChanged: (value) => updateList(value),
                     decoration: InputDecoration(
-                        hintText: '   search pokemon to name',
+                        hintText: '   search pokemon to num or name',
                         prefixIcon: Icon(Icons.search),
                         prefixIconColor: Colors.white,
                         filled: true,
@@ -93,8 +112,18 @@ class _PoketListState extends State<PoketList> {
                             return Card(
                               child: ListTile(
                                 //   title: Text('일러'),
-                                leading: Text("사진"),
-                                trailing: Text('타입아이콘'),
+                                leading:
+                                    Text("도감번호 : " + (index + 1).toString()),
+                                trailing: IconButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => Listdetail()),
+                                    );
+                                  },
+                                  icon: Icon(Icons.navigate_next),
+                                ),
                                 title: Center(
                                   child: Text(
                                     pokename.toString(),
--- lib/listdetail.dart
+++ lib/listdetail.dart
@@ -1,11 +1,56 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_const_constructors_in_immutables, prefer_typing_uninitialized_variables, annotate_overrides, prefer_interpolation_to_compose_strings
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
-class Listdetail extends StatelessWidget {
-  const Listdetail({super.key});
+class PokeApi {
+  var types;
+  var name;
+
+  PokeApi({
+    required this.types,
+    required this.name,
+  });
+
+  factory PokeApi.fromJson(Map<String, dynamic> json) {
+    return PokeApi(
+      types: json['types'],
+      name: json['name'],
+    );
+  }
+}
+
+Future<PokeApi> fetchAlbum() async {
+  // 포켓몬 주소
+  final response =
+      await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon/1/'));
+
+  if (response.statusCode == 200) {
+    return PokeApi.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load pokeApi');
+  }
+}
+
+class Listdetail extends StatefulWidget {
+  Listdetail({super.key});
 
   @override
+  State<Listdetail> createState() => _ListdetailState();
+}
+
+class _ListdetailState extends State<Listdetail> {
+  late Future<PokeApi> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+    // futurepage = nextpage();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -23,12 +68,10 @@ class Listdetail extends StatelessWidget {
                 child: PageView(
                   children: [
                     Container(
-                      child: Icon(
-                        Icons.square,
-                        size: 150,
-                        color: Colors.blue,
-                      ),
-                    ),
+                        child: Image(
+                      image: NetworkImage(
+                          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png"),
+                    )),
                     Container(
                       child: Icon(
                         Icons.square,
@@ -59,8 +102,31 @@ class Listdetail extends StatelessWidget {
                         decoration: BoxDecoration(
                           border: Border.all(),
                         ),
-                        child: ListTile(
-                          leading: Text('국가별 이름'),
+                        child: SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.1,
+                          child: FutureBuilder<PokeApi>(
+                            future: futureAlbum,
+                            builder: (context, snapshot) {
+                              if (snapshot.hasData) {
+                                return Center(
+                                  child: Row(
+                                    children: [
+                                      SizedBox(
+                                        width: 50,
+                                      ),
+                                      Text(snapshot.data!.name.toString()),
+                                    ],
+                                  ),
+                                );
+                                //return Text(snapshot.data!.types[0].toString());
+                              } else if (snapshot.hasError) {
+                                return Text('${snapshot.error}');
+                              }
+
+                              // By default, show a loading spinner.
+                              return Center(child: CircularProgressIndicator());
+                            },
+                          ),
                         ),
                       ),
                     ),
@@ -92,8 +158,41 @@ class Listdetail extends StatelessWidget {
                         decoration: BoxDecoration(
                           border: Border.all(),
                         ),
-                        child: ListTile(
-                          leading: Text('타입 및 타입상성'),
+                        child: SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.1,
+                          child: FutureBuilder<PokeApi>(
+                            future: futureAlbum,
+                            builder: (context, snapshot) {
+                              if (snapshot.hasData) {
+                                return Center(
+                                  child: Row(
+                                    children: [
+                                      SizedBox(
+                                        width: 50,
+                                      ),
+                                      Text('타입 1 : '),
+                                      Text(
+                                        snapshot.data!.types[0]["type"]["name"]
+                                                .toString() +
+                                            "   ,  ",
+                                      ),
+                                      Text('타입 2 : '),
+                                      Text(
+                                        snapshot.data!.types[1]["type"]["name"]
+                                            .toString(),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                                //return Text(snapshot.data!.types[0].toString());
+                              } else if (snapshot.hasError) {
+                                return Text('${snapshot.error}');
+                              }
+
+                              // By default, show a loading spinner.
+                              return Center(child: CircularProgressIndicator());
+                            },
+                          ),
                         ),
                       ),
                     ),
